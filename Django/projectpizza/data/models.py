de600288c9b5e083918c3cdbebeb9b06fb47f09a
saintty <aauzinger@yandex.ru>
2021-11-22 18:27:38 +0300

    Models(Product, Category) Creation

--- Django/projectpizza/data/models.py
+++ Django/projectpizza/data/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    productName = models.CharField(max_length=50)
+    productDescription = models.CharField(max_length=300)
+    productIngredients = models.CharField(max_length=300)
+    productPrice = models.FloatField()
+    productPhoto = models.ImageField()
+    productCategory = models.ForeignKey('Category', on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.productName
+
+class Category(models.Model):
+    categoryName = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.categoryName
+

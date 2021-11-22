576f47fda70a398a9d48a4ba08b69f0c2fcdc36a
saintty <aauzinger@yandex.ru>
2021-11-22 19:32:55 +0300

    Site Template creation

--- Django/projectpizza/data/views.py
+++ Django/projectpizza/data/views.py
@@ -3,5 +3,32 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from .models import *
+
+siteNavigation = [
+    "О нас",
+    "Акции",
+    "Карта",
+    "Еще",
+    "Чтото",
+]
+productCategory = [
+    "Pizza",
+    "Rolls",
+    "Drinks",
+    "Ice-cream",
+]
+
+
+
 def mainPage(request):
-    return HttpResponse('Базовая страница сайта')
+    products = Product.objects.all()
+    print(products)
+    
+    return render(request, 'data/siteMainPage.html', {
+        # передача данных по имени, на основании которых строится представление на сайте
+        'title':'.projectPizza',
+        'siteNavigation':siteNavigation,
+        'productCategory':productCategory,
+        'items':products
+    })

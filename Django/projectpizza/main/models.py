a1a62656c5859083f6d81c45772a46a3b1e92ea7
saintty <aauzinger@yandex.ru>
2021-12-23 20:43:25 +0300

    create cat model

--- Django/projectpizza/main/models.py
+++ Django/projectpizza/main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.deletion import PROTECT
 
 # Create your models here.
 
@@ -6,6 +7,14 @@ class Product(models.Model):
     name = models.CharField(max_length=50)
     discription = models.CharField(max_length=255)
     cost = models.FloatField()
+    category = models.ForeignKey("ProductCategory", on_delete=PROTECT, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=50)
     
     def __str__(self):
         return self.name

db93f7d49024c4aecb9088cc379c73cfee1e80ac
saintty <aauzinger@yandex.ru>
2021-12-28 19:14:09 +0300

    db change

--- Django/projectpizza/projectpizza/settings.py
+++ Django/projectpizza/projectpizza/settings.py
@@ -78,10 +78,14 @@ WSGI_APPLICATION = 'projectpizza.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
+DATABASES ={
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'pizzaproject',
+        'USER': 'root',
+        'PASSWORD': 'pizza123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 

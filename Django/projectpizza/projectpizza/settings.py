2f58adc62e0f8fa18b18aa8ab02bc6bb06b4f0a2
saintty <aauzinger@yandex.ru>
2022-01-04 16:17:31 +0300

    email smtp add

--- Django/projectpizza/projectpizza/settings.py
+++ Django/projectpizza/projectpizza/settings.py
@@ -156,6 +156,11 @@ MEDIA_URL = '/media/'
 CART_SESSION_ID = 'cart'
 AUTH_USER_MODEL = 'users.CustomUser'
 
-EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
-EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'our.project.pizza@gmail.com'
+EMAIL_HOST_PASSWORD = 'Project_Pizza_123'
 
+#DEFAULT_FROM_EMAIL = EMAIL_HOST_USER

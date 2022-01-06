daa889572c77c99be0eeaf8563a101ae30004a31
admiralKucha <91753927+admiralKucha@users.noreply.github.com>
2022-01-06 23:07:49 +0300

    вроде работает
    Основная работа с базами данных

--- telegram bot/database.py
+++ telegram bot/database.py
@@ -1,17 +1,208 @@
-import sqlite3 as sql3
+import psycopg2 as db
+import logging
 from hashlib import sha256
 
 def signIn(login, password):
-    con = sql3.connect("C:/Users/Максим/Documents/GitHub/project-pizza/telegram bot/db4.sqlite3")
+    logging.basicConfig(filename="log_error_telegram.log", filemode='a', level=logging.ERROR, format = "%(asctime)s - %(levelname)s - %(funcName)s: %(lineno)d - %(message)s")
+    try:
+        con = db.connect(user="postgres",
+                        password="1234",
+                        host="localhost",
+                        port="5432",
+                        database="postgres")
+    except:
+        return ()
+    
     cursor = con.cursor()
     hash_object = sha256(password.encode())
     hash_object = hash_object.hexdigest()
-    sql = "SELECT * FROM auth_user WHERE password=? AND email=?"
-    cursor.execute(sql, (hash_object, login))
-    res = cursor.fetchall()
-    if res != []:
-        return True
+    sql = "SELECT * FROM users_customuser WHERE email = %s AND password = %s"
+    cursor.execute(sql, [login, hash_object])
+    res = cursor.fetchone()
+    cursor.close()
+    con.close()
+    if res != None:
+        return res[0]
     else:
-        return False
-        
-print(signIn("projectpizza@dev.il", "1234"))
+        return ()
+
+def orders(user):
+    try:
+        con = db.connect(user="postgres",
+                        password="1234",
+                        host="localhost",
+                        port="5432",
+                        database="postgres")
+    except:
+        return ()
+
+    cursor = con.cursor()
+    sql = "SELECT * FROM orders_order WHERE user_id = %s"
+    cursor.execute(sql, [user])
+    res = cursor.fetchall()
+    cursor.close()
+    con.close()
+    if res != []:
+        string = ""
+        for i in res: 
+            string = string + "Заказ номер /" + str(i[0]) + " на сумму " + str(i[-2]) + "\n"
+        return string
+    else:
+        return -1
+
+def openOrders(number, user):
+    try:
+        con = db.connect(user="postgres",
+                        password="1234",
+                        host="localhost",
+                        port="5432",
+                        database="postgres")
+    except:
+        return ()
+    cursor = con.cursor()
+    sql = "SELECT * FROM orders_order WHERE id = %s AND user_id =  %s"
+    cursor.execute(sql, [str(number), user])
+    res = cursor.fetchone()
+    if res == None:
+        return "Неверный номер!"
+
+    sql = "SELECT * FROM orders_orderitems WHERE order_id = %s"
+    cursor.execute(sql, str(number))
+    res = cursor.fetchall()
+    string =""
+    for i in res:
+        sql = "SELECT * FROM main_product WHERE id = %s"
+        cursor.execute(sql, str(i[-1]))
+        name = cursor.fetchone()
+        string = string + str(i[2]) + " \"" + name[1] + "\" на сумму " + str(i[1]) + "\n"
+    cursor.close()
+    con.close()
+    return string
+
+def promo(user):
+    try:
+        con = db.connect(user="postgres",
+                        password="1234",
+                        host="localhost",
+                        port="5432",
+                        database="postgres")
+    except:
+        return ()
+
+    cursor = con.cursor()
+    sql = "SELECT * FROM promo WHERE user_id = %s"
+    cursor.execute(sql, [user])
+    res = cursor.fetchall()
+    cursor.close()
+    con.close()
+    if res != []:
+        string = ""
+        for i in res: 
+            string = string + "Акция номер /" + str(i[0]) + "\n"
+        return string
+    else:
+        return -1
+
+def openPromo(number, user):
+    try:
+        con = db.connect(user="postgres",
+                        password="1234",
+                        host="localhost",
+                        port="5432",
+                        database="postgres")
+    except:
+        return ()
+    cursor = con.cursor()
+    sql = "SELECT * FROM promo WHERE id = %s AND user_id = %s"
+    cursor.execute(sql, [str(number), user])
+    res = cursor.fetchone()
+    if res == None:
+        return "Неверный номер!"
+    string =""
+    sql = "SELECT * FROM main_product WHERE id = %s"
+    cursor.execute(sql, str(res[2]))
+    name = cursor.fetchone()
+    string = string + "Акция номер /" + str(res[0]) + "\n" + "На \"" + name[1] + "\"" + "\n" + "Cкидка " + str(res[3]) + "%" + "\n"  + "До " + str(res[4])
+    cursor.close()
+    con.close()
+    return string
+
+def menu():
+    try:
+        con = db.connect(user="postgres",
+                        password="1234",
+                        host="localhost",
+                        port="5432",
+                        database="postgres")
+    except:
+        return ()
+
+    cursor = con.cursor()
+    sql = "SELECT * FROM main_productcategory "
+    cursor.execute(sql)
+    res = cursor.fetchall()
+    string = ""
+    for i in res:
+        string = string + "/" + str(i[0]) + " " + i[1] +  "\n"
+    cursor.close()
+    con.close()
+    return string
+
+def nextMenu(product, page):
+    try:
+        con = db.connect(user="postgres",
+                        password="1234",
+                        host="localhost",
+                        port="5432",
+                        database="postgres")
+    except:
+        return ()
+    cursor = con.cursor()
+    sql = "SELECT * FROM main_product WHERE category_id = %s"
+    cursor.execute(sql, str(product))
+    res = cursor.fetchall()
+    string = ""
+    #flag -1 отключаем левый переход, 0 нормальное состояние, 1 отключаем правый переход
+    if len(res) < page*4+5:
+        len_end = len(res)
+        flag = 1
+    else:
+        len_end = page*4+4
+        flag = 0
+    if page == 0:
+        flag = -1
+    len_start = page*4
+    for i in range (len_start, len_end):
+        string = string + "/" + str(i+1) + " " + res[i][1] +  "\n"
+    cursor.close()
+    con.close()
+    return [string, flag]
+
+def product(number, product) :
+    try:
+        con = db.connect(user="postgres",
+                        password="1234",
+                        host="localhost",
+                        port="5432",
+                        database="postgres")
+    except:
+        return ()
+    cursor = con.cursor()
+    sql = "SELECT * FROM main_product WHERE category_id = %s"
+    cursor.execute(sql, str(product))
+    res = cursor.fetchall()
+    string = ""
+    number = int(number)
+    if len(res) < number:
+        return "Нету такого номера!"
+    else:
+        res = res[number]
+        string =  res[1] + "\n"+ "Стоит " + str(res[3]) 
+        res = res[2]
+        if len(res) == 1:
+            return string
+        string   = string + "\n" + "Состав:" + "\n"
+        for i in res:
+            string = string + i
+        return string
+
--- telegram bot/telegram_bot.py
+++ telegram bot/telegram_bot.py
@@ -1,5 +1,6 @@
 import logging
-from database import signIn
+from database import signIn, orders, openOrders, promo, openPromo, menu, nextMenu, product
+from joke import joke
 #основа
 from aiogram import Bot
 from aiogram.dispatcher import Dispatcher, FSMContext
@@ -14,14 +15,18 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage #состояния
 #файловый импорт
 from config import TOKEN
 from states import TestStates
-from buttons import greet, greet_menu, greet_post_login, greet_acc
+from buttons import greet, greet_menu, greet_post_login, greet_acc, greet_order, greet_product_menu, greet_product_menu_w_l, greet_product_menu_w_n
 from stringPizza import success_str_log, fail_str_log, password_str, help_string, login_string, acc_string
 from stringPizza import start1, start2, start3, miss
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
+
+#Cловари
 dic_id = dict()
 dict_log = dict()
+dict_user = dict()
+dict_menu = dict()
 
 logging.basicConfig(filename="log_error_telegram.log", filemode='a', level=logging.ERROR, format = "%(asctime)s - %(levelname)s - %(funcName)s: %(lineno)d - %(message)s")
 
@@ -36,7 +41,6 @@ async def state_password(message: Message):
     flag = signIn(dict_log[msg_us_id], message.text)
     del dict_log[msg_us_id]
     del dic_id[msg_us_id]
-
     try:
         await bot.delete_message(message_id = smth, chat_id = msg_us_id)
     except:
@@ -45,9 +49,10 @@ async def state_password(message: Message):
                 await bot.delete_message(message_id = i, chat_id = msg_us_id)
             except:
                 continue
-            if flag: 
+            if flag != (): 
+                dict_user[msg_us_id] = flag
                 await message.answer(success_str_log, reply_markup = greet_post_login)
-                await state.set_state(TestStates.all()[1])
+                await state.set_state(TestStates.all()[2])
             else:               
                 await message.answer("НЕВЕРНО", reply_markup = greet)
                 await state.reset_state()
@@ -58,9 +63,10 @@ async def state_password(message: Message):
             await state.reset_state()
 
     else:
-        if flag:
+        if flag != ():
             await message.answer(success_str_log, reply_markup = greet_post_login)
-            await state.set_state(TestStates.all()[1])
+            dict_user[msg_us_id] = flag
+            await state.set_state(TestStates.all()[2])
         else:
             await message.answer("НЕВЕРНО", reply_markup = greet)
             await state.reset_state()
@@ -85,7 +91,7 @@ async def state_login(message: Message):
             except:
                 continue
 
-            await state.set_state(TestStates.all()[3])
+            await state.set_state(TestStates.all()[4])
             await message.answer(password_str)
             break
 
@@ -94,36 +100,273 @@ async def state_login(message: Message):
             await state.reset_state()
             
     else:
-        await state.set_state(TestStates.all()[3])
+        await state.set_state(TestStates.all()[4])
         await message.answer(password_str)
 
 
 @dp.message_handler(state=TestStates.TEST_STATE_ACC)
 async def state_acc(message: Message):
     state = dp.current_state()
- 
     if message.text == "exit":
         state = dp.current_state()
         string = "Успешно выполнен выход из аккаунта"
+        del dict_user[message.from_user.id]
         await state.reset_state()
         await message.answer(string, reply_markup = greet)
 
     elif message.text == 'помощь':
-        await state.set_state(TestStates.all()[1])
+        await state.set_state(TestStates.all()[2])
         await message.answer(help_string,  reply_markup = greet_post_login, parse_mode=ParseMode.MARKDOWN)
 
     elif message.text == 'заказы':
-        string = "*Заглушка*"
-        await message.answer(string, reply_markup = greet_acc)
-
+        res = orders(dict_user[message.from_user.id])
+        if res == ():
+            await message.answer("Ошибка, попробуйте позже", reply_markup = greet_acc)
+        elif res == -1:
+            await message.answer("Заказов нет(((", reply_markup = greet_acc)
+        else:
+            await state.set_state(TestStates.all()[1])
+            await message.answer(res, reply_markup = greet_order)
+        
     elif message.text == 'акции':
-        string = "*Заглушка*"
-        await message.answer(string, reply_markup = greet_acc)
+        res = promo(dict_user[message.from_user.id])
+        if res == ():
+            await message.answer("Ошибка, попробуйте позже", reply_markup = greet_acc)
+        elif res == -1:
+            await message.answer("Акции закончились(((", reply_markup = greet_acc)
+        else:
+            await state.set_state(TestStates.all()[5])
+            await message.answer(res, reply_markup = greet_order)
+    else:
+        await message.answer(miss,  reply_markup = greet_acc)
+
+
+@dp.message_handler(state=TestStates.TEST_STATE_ACC_ORDER)
+async def state_acc_order(message: Message):
+    state = dp.current_state()
+    if message.text == 'помощь':
+        await state.set_state(TestStates.all()[2])
+        await message.answer(help_string,  reply_markup = greet_post_login, parse_mode=ParseMode.MARKDOWN)
+        return
+
+    elif  message.text == 'аккаунт':
+        state = dp.current_state()
+        await state.set_state(TestStates.all()[0])
+        await message.answer(acc_string, reply_markup = greet_acc, parse_mode=ParseMode.MARKDOWN) 
+        return
+
+    res = openOrders(message.text[1:], dict_user[message.from_user.id])
+    if res == ():
+        await message.answer("Ошибка, попробуйте позже", reply_markup = greet_acc)
+    else:
+        await message.answer(res, reply_markup = greet_order)
+
+@dp.message_handler(state=TestStates.TEST_STATE_PROMO)
+async def state_acc_order(message: Message):
+    state = dp.current_state()
+    if message.text == 'помощь':
+        await state.set_state(TestStates.all()[2])
+        await message.answer(help_string,  reply_markup = greet_post_login, parse_mode=ParseMode.MARKDOWN)
+        return
+
+    elif  message.text == 'аккаунт':
+        state = dp.current_state()
+        await state.set_state(TestStates.all()[0])
+        await message.answer(acc_string, reply_markup = greet_acc, parse_mode=ParseMode.MARKDOWN) 
+        return
+
+    res = openPromo(message.text[1:], dict_user[message.from_user.id])
+    if res == ():
+        await message.answer("Ошибка, попробуйте позже", reply_markup = greet_acc)
+    else:
+        await message.answer(res, reply_markup = greet_order)
+
+@dp.message_handler(state=TestStates.TEST_STATE__MENU)
+async def state_acc_order(message: Message):
+    state = dp.current_state()
+    if message.text == 'помощь':
+        if dict_user.get(message.from_user.id) == None:
+            await state.reset_state()
+            await message.answer(help_string,  reply_markup = greet, parse_mode=ParseMode.MARKDOWN)
+        else:
+            await state.set_state(TestStates.all()[2])
+            await message.answer(help_string,  reply_markup = greet_post_login, parse_mode=ParseMode.MARKDOWN)
+        return
+
+    elif  message.text == 'меню':
+        string = menu()
+        if string == ():
+            await message.answer("Ошибка, попробуйте позже",  reply_markup = greet_menu)
+            return
+        dict_menu[message.from_user.id] = 0
+        await state.set_state(TestStates.all()[6])
+        await message.answer(string,  reply_markup = greet_menu) 
+        return
+
+    #ТУТ НАДО ПРОВЕРКУ ПОСТАВИТЬ ЧТО ДАЛИ КОРЕКТНОЕ ЧИСЛО
+    res, flag = nextMenu(message.text[1:], dict_menu[message.from_user.id])
+    if res == ():
+        await message.answer("Ошибка, попробуйте позже", reply_markup = greet_menu)
+    elif res == "":
+        await message.answer("Неправильная позиция", reply_markup = greet_menu)
+    else:
+        if flag == -1:
+            greet_now = greet_product_menu_w_l
+            await state.set_state(TestStates.all()[8])
+        elif flag == 0:
+            greet_now = greet_product_menu
+            await state.set_state(TestStates.all()[7])
+        else:
+            greet_now = greet_product_menu_w_n
+            await state.set_state(TestStates.all()[9])
+
+        dict_menu[message.from_user.id] = [message.text[1:], dict_menu[message.from_user.id]]
+        await message.answer(res, reply_markup = greet_now)
+
+@dp.message_handler(state=TestStates.TEST_STATE__MENU_PRODUCT)
+async def state_acc_order(message: Message):
+    state = dp.current_state()
+    if message.text == 'помощь':
+        if dict_user.get(message.from_user.id) == None:
+            await state.reset_state()
+            await message.answer(help_string,  reply_markup = greet, parse_mode=ParseMode.MARKDOWN)
+        else:
+            await state.set_state(TestStates.all()[2])
+            await message.answer(help_string,  reply_markup = greet_post_login, parse_mode=ParseMode.MARKDOWN)
+        return
+
+    elif  message.text == 'меню':
+        string = menu()
+        if string == ():
+            await message.answer("Ошибка, попробуйте позже",  reply_markup = greet_menu)
+            return
+        dict_menu[message.from_user.id] = 0
+        await state.set_state(TestStates.all()[6])
+        await message.answer(string,  reply_markup = greet_menu) 
+        return
+    elif len(message.text) > 10:
+        if message.text == 'следующая страница':
+            dict_menu[message.from_user.id][1] = dict_menu[message.from_user.id][1] + 1
+            res, flag = nextMenu(dict_menu[message.from_user.id][0], dict_menu[message.from_user.id][1])
+
+        elif message.text == 'прошлая страница':
+            dict_menu[message.from_user.id][1] = dict_menu[message.from_user.id][1] - 1
+            res, flag = nextMenu(dict_menu[message.from_user.id][0], dict_menu[message.from_user.id][1])
+        if res == ():
+            await message.answer("Ошибка, попробуйте позже", reply_markup = greet_menu)
+        else:
+            if flag == -1:
+                greet_now = greet_product_menu_w_l
+                await state.set_state(TestStates.all()[8])
+            elif flag == 0:
+                greet_now = greet_product_menu
+                await state.set_state(TestStates.all()[7])
+            else:
+                greet_now = greet_product_menu_w_n
+                await state.set_state(TestStates.all()[9])
+
+            await message.answer(res, reply_markup = greet_now)
+    
+    elif message.text[0] == "/":
+        res = product(message.text[1:], dict_menu[message.from_user.id][0])
+        await message.answer(res, reply_markup = greet_product_menu)
+
+@dp.message_handler(state=TestStates.TEST_STATE__MENU_PRODUCT_W_L)
+async def state_acc_order(message: Message):
+    state = dp.current_state()
+    if message.text == 'помощь':
+        if dict_user.get(message.from_user.id) == None:
+            await state.reset_state()
+            await message.answer(help_string,  reply_markup = greet, parse_mode=ParseMode.MARKDOWN)
+        else:
+            await state.set_state(TestStates.all()[2])
+            await message.answer(help_string,  reply_markup = greet_post_login, parse_mode=ParseMode.MARKDOWN)
+        return
+
+    elif  message.text == 'меню':
+        string = menu()
+        if string == ():
+            await message.answer("Ошибка, попробуйте позже",  reply_markup = greet_menu)
+            return
+        dict_menu[message.from_user.id] = 0
+        await state.set_state(TestStates.all()[6])
+        await message.answer(string,  reply_markup = greet_menu) 
+        return
+
+    elif message.text == 'следующая страница':
+        dict_menu[message.from_user.id][1] = dict_menu[message.from_user.id][1] + 1
+        res, flag = nextMenu(dict_menu[message.from_user.id][0], dict_menu[message.from_user.id][1])
+        if res == ():
+            await message.answer("Ошибка, попробуйте позже", reply_markup = greet_menu)
+        else:
+            if flag == -1:
+                greet_now = greet_product_menu_w_l
+                await state.set_state(TestStates.all()[8])
+            elif flag == 0:
+                greet_now = greet_product_menu
+                await state.set_state(TestStates.all()[7])
+            else:
+                greet_now = greet_product_menu_w_n
+                await state.set_state(TestStates.all()[9])
+
+            await message.answer(res, reply_markup = greet_now)
+
+    elif message.text[0] == "/":
+        res = product(message.text[1:], dict_menu[message.from_user.id][0])
+        await message.answer(res, reply_markup = greet_product_menu_w_l)
+    else:
+        await message.answer(miss,  reply_markup = greet_menu)
+
+@dp.message_handler(state=TestStates.TEST_STATE__MENU_PRODUCT_W_N)
+async def state_acc_order(message: Message):
+    state = dp.current_state()
+    if message.text == 'помощь':
+        if dict_user.get(message.from_user.id) == None:
+            await state.reset_state()
+            await message.answer(help_string,  reply_markup = greet, parse_mode=ParseMode.MARKDOWN)
+        else:
+            await state.set_state(TestStates.all()[2])
+            await message.answer(help_string,  reply_markup = greet_post_login, parse_mode=ParseMode.MARKDOWN)
+        return
+
+    elif  message.text == 'меню':
+        string = menu()
+        if string == ():
+            await message.answer("Ошибка, попробуйте позже",  reply_markup = greet_menu)
+            return
+        dict_menu[message.from_user.id] = 0
+        await state.set_state(TestStates.all()[6])
+        await message.answer(string,  reply_markup = greet_menu) 
+        return
+
+    elif message.text == 'прошлая страница':
+        dict_menu[message.from_user.id][1] = dict_menu[message.from_user.id][1] - 1
+        res, flag = nextMenu(dict_menu[message.from_user.id][0], dict_menu[message.from_user.id][1])
+        if res == ():
+            await message.answer("Ошибка, попробуйте позже", reply_markup = greet_menu)
+        else:
+            if flag == -1:
+                greet_now = greet_product_menu_w_l
+                await state.set_state(TestStates.all()[8])
+            elif flag == 0:
+                greet_now = greet_product_menu
+                await state.set_state(TestStates.all()[7])
+            else:
+                greet_now = greet_product_menu_w_n
+                await state.set_state(TestStates.all()[9])
+
+            await message.answer(res, reply_markup = greet_now)
+
+    elif message.text[0] == "/":
+        res = product(message.text[1:], dict_menu[message.from_user.id][0])
+        await message.answer(res, reply_markup = greet_product_menu_w_n)
+    else:
+        await message.answer(miss,  reply_markup = greet_menu)      
 
 @dp.message_handler(state='*')
 async def main(message: Message, state: FSMContext):
     state = await state.get_state()
-    if TestStates.all()[1] == state:
+    if TestStates.all()[2] == state:
         greet_now = greet_post_login
     else:
         greet_now = greet
@@ -140,37 +383,33 @@ async def main(message: Message, state: FSMContext):
 
     elif message.text == 'помощь':
         await message.answer(help_string,  reply_markup = greet_now, parse_mode=ParseMode.MARKDOWN)
-    #эх отдельное состояние надо прописать
+
     elif message.text == "меню":
-        string = "*Заглушка (здесь должна использоваться бд)*" + "\n"
+        string = menu()
+        if string == ():
+            await message.answer("Ошибка, попробуйте позже",  reply_markup = greet_menu)
+            return
+        state = dp.current_state()
+        dict_menu[message.from_user.id] = 0
+        await state.set_state(TestStates.all()[6])
         await message.answer(string,  reply_markup = greet_menu) 
 
-    elif message.text == 'покажи все меню':
-        string = "*Заглушка*"
-        await message.answer(string,  reply_markup = greet_menu)
-
-    elif message.text == 'вся пицца':
-        string = ("*Заглушка*")
-        await message.answer(string, reply_markup = greet_menu)
-
     elif message.text == 'анекдот':
-        string = "*Заглушка (здесь должна использоваться бд)*"
-        await message.answer(string,  reply_markup = greet_now)
-        #здесь надо сделать обращение к базе данных, а не вот это вот
+        await message.answer(joke(),  reply_markup = greet_now)
 
-    elif message.text == 'login' and state != TestStates.all()[1]:
+    elif message.text == 'login' and state != TestStates.all()[2]:
         dic_id[message.from_user.id] = message.message_id+1
         state = dp.current_state()
-        await state.set_state(TestStates.all()[2])
+        await state.set_state(TestStates.all()[3])
         await message.answer(login_string, reply_markup = ReplyKeyboardRemove())
 
-    elif  message.text == 'аккаунт' and state == TestStates.all()[1]:
+    elif  message.text == 'аккаунт' and state == TestStates.all()[2]:
         state = dp.current_state()
         await state.set_state(TestStates.all()[0])
         await message.answer(acc_string, reply_markup = greet_acc, parse_mode=ParseMode.MARKDOWN)
         
     else:  
-        await message.answer(miss,  reply_markup = greet)
+        await message.answer(miss,  reply_markup = greet_now)
 
 if __name__ == '__main__':
     executor.start_polling(dp)

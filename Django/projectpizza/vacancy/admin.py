137eae36d64bca80ea89357ed1dd21fc2484b3d5
saintty <aauzinger@yandex.ru>
2022-01-19 00:54:36 +0300

    vacancy upd

--- Django/projectpizza/vacancy/admin.py
+++ Django/projectpizza/vacancy/admin.py
@@ -4,7 +4,7 @@ from .models import Vacancy
 # Register your models here.
 
 class VacancyAdmin(admin.ModelAdmin):
-    list_display = ('id', 'profession', 'discription', 'salary', 'is_open', 'image')
+    list_display = ('id', 'profession', 'discription', 'salary', 'is_open')
     search_fields = ('profession', 'salary', 'is_open')
 
 admin.site.register(Vacancy, VacancyAdmin)
--- Django/projectpizza/vacancy/models.py
+++ Django/projectpizza/vacancy/models.py
@@ -6,7 +6,6 @@ class Vacancy(models.Model):
     profession = models.CharField(max_length=50, verbose_name='Профессия')
     discription = models.CharField(max_length=400, verbose_name='Предложение')
     salary = models.IntegerField(default=10000, verbose_name='Заработная плата')
-    image = models.ImageField(upload_to='VacancyImages', verbose_name='Фото')
     is_open = models.BooleanField(default=True, verbose_name='Актуальность')
     
     def __str__(self):

a2044f6a3c6c0d580db98d6bfe535bea56f79ef5
Mikhail Shornikov <93269956+mshornikov@users.noreply.github.com>
2022-01-19 02:43:02 +0300

    Design Update #2.5 (Basket, menu, combo, registratio, and other)

--- Django/projectpizza/orders/models.py
+++ Django/projectpizza/orders/models.py
@@ -10,7 +10,7 @@ class Order(models.Model):
     first_name = models.CharField(max_length=50, verbose_name='Имя')
     last_name = models.CharField(max_length=50, verbose_name='Фамилия')
     email = models.EmailField(verbose_name='Почта')
-    address = models.CharField(max_length=300, verbose_name='Адресс')
+    address = models.CharField(max_length=300, verbose_name='Адрес')
     city = models.CharField(max_length=50, verbose_name='Город')
     created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     total_cost = models.DecimalField(default=0, max_digits=10, decimal_places=2, verbose_name='Сумма заказа')
